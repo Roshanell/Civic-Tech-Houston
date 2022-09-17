ff0e7ee9ca4c325c0c06b8f1219a2c24735de7f5
Roshanell <96213223+Roshanell@users.noreply.github.com>
2022-09-17 17:35:52 -0500

    add zipcode field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider, Code, FormControl, FormLabel, Input, FormHelperText,} from "@chakra-ui/react";
 
 function App() {
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <FormControl>
+        <FormLabel>Enter Your Zip Code</FormLabel>
+        <Input type="email" />
+        <FormHelperText>We'll never share your personal information.</FormHelperText>
+      </FormControl>
+    </ChakraProvider>
   );
 }
-
 export default App;
