7b39cb7a60d8a432826259a1a567a160b942e77a
Roshanell <96213223+Roshanell@users.noreply.github.com>
2022-09-18 22:38:34 -0500

    updated inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import * as React from "react";
+import superNeighborhoods from "./data.json";
 
 // 1. import `ChakraProvider` component
 import {
   ChakraProvider,
-  Code,
   FormControl,
   FormLabel,
   Input,
@@ -15,27 +15,55 @@ import {
 } from "@chakra-ui/react";
 
 function App() {
+  const [address, setAddress] = React.useState("");
+
+  const [selectedSuperNeighborhood] = superNeighborhoods.filter(
+    (superNeighborhood) => {
+      return superNeighborhood.address === address;
+    }
+  );
+
+  const handleChange = (event) => {
+    setAddress(event.target.value);
+  };
+
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
       <Box m={100}>
         <FormControl>
-          <FormLabel>Enter Your Zip Code</FormLabel>
-          <Input type="email" />
+          <FormLabel>Enter Your Address</FormLabel>
+          <Input value={address} onChange={handleChange} />
           <FormHelperText>
             We'll never share your personal information.
           </FormHelperText>
         </FormControl>
-        <Stack spacing={3} m={50}>
-          <Text fontSize="4xl"> Civic Information </Text>
-          <Text fontSize="3xl">Name: Midtown Super Neighborhood #62</Text>
-          <Text fontSize="3xl">Times: 6:30 PM to 8:00 PM </Text>
-          <Text fontSize="3xl">Dates: Second Thursday of the month</Text>
-          <Text fontSize="3xl">Location: 3001 Main St, Houston, TX 77004</Text>
-        </Stack>
-        <Button colorScheme="blue" spacing={3} ml={52}>
-          Report a Problem
-        </Button>
+        {selectedSuperNeighborhood ? (
+          <>
+            <Stack spacing={3} m={50}>
+              <Text fontSize="4xl"> Civic Information </Text>
+              <Text fontSize="3xl">
+                Name: {selectedSuperNeighborhood?.name}
+              </Text>
+              <Text fontSize="3xl">
+                Status: {selectedSuperNeighborhood?.status}
+              </Text>
+              {selectedSuperNeighborhood?.contactAndMeetingInformation ? (
+                <Text fontSize="3xl">
+                  Dates:
+                  {selectedSuperNeighborhood?.contactAndMeetingInformation}{" "}
+                </Text>
+              ) : null}
+              <Text fontSize="3xl">
+                Location: {selectedSuperNeighborhood?.location}
+              </Text>
+            </Stack>
+
+            <Button colorScheme="blue" spacing={3} ml={52}>
+              Report a Problem
+            </Button>
+          </>
+        ) : null}
       </Box>
     </ChakraProvider>
   );
